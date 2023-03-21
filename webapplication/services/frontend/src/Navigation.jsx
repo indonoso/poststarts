c7d15739fae9880e7ba1811fe5a64f770df5db9f
Ivania Donoso-Guzmán <ivania.donoso@gmail.com>
2023-03-21 23:29:30 +0100

    Added home page, navigation and routing

---
+++ webapplication/services/frontend/src/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Postcards Dataset</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+
+                        <Nav.Link as={Link} to="/search">Search</Nav.Link>
+
+                        <NavDropdown title="Data Analysis" id="basic-nav-dropdown">
+
+                            <NavDropdown.Item as={Link} to="/exploratory_analysis">
+                                Exploratory Analysis
+                            </NavDropdown.Item>
+
+                            <NavDropdown.Item as={Link} to="/geospatial_data">
+                                Geo Spatial Data
+                            </NavDropdown.Item>
+
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Navigation;
--- webapplication/services/frontend/src/index.js
+++ webapplication/services/frontend/src/index.js
@@ -1,7 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import ExploratoryDataAnalysis from "./pages/ExploratoryDataAnalysis";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/exploratory_data_analysis" element={<ExploratoryDataAnalysis />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
