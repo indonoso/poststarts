68de32d6c24111c2b1d047d71e1ab938de0db7b4
Ivania Donoso-Guzmán <ivania.donoso@gmail.com>
2023-03-21 20:45:29 +0100

    Added search engine backend

---
+++ webapplication/services/backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from text2image_search import Text2ImageSearch, DataBase
+# create a Flask instance
+app = Flask(__name__)
+
+
+database = DataBase('metadata.csv', 'vectors.npy')
+search_engine = Text2ImageSearch(database.vectors)
+
+
+@app.route('/search/<query>', methods=['GET'])
+def search(query):
+    hits = search_engine.search(query, k=10)
+    return database.get_data_for(hits, columns=['title', 'public_url'])
+
+
+if __name__ == "__main__":
+    # for debugging locally
+    # app.run(debug=True, host='0.0.0.0',port=5000)
+
+    # for production
+    app.run(host='0.0.0.0', port=5000)
--- webapplication/services/backend/manage.py
+++ webapplication/services/backend/manage.py
@@ -1,27 +1,9 @@
-# manage.py
+from app import app
 
-from app import create_app,db
-from flask.cli import FlaskGroup
-from helper.models import Message
-
-app = create_app()
-cli = FlaskGroup(create_app=create_app)
-
-@cli.command('populate_db')
-def populate_db():
-    "populate reseted database" 
-    message = Message(text="First message!")
-    db.session.add(message)
-    db.session.commit()
-    print("database creation completed")
-
-@cli.command('reset_db')
-def recreate_db():
-    """delete and reset database"""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-    print("database reset done!")
 
 if __name__ == "__main__":
-    cli()
+    # for debugging locally
+    app.run(debug=True, host='0.0.0.0',port=5000)
+
+    # for production
+    # app.run(host='0.0.0.0', port=5000)
